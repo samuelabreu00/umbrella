dc2ec3e6030f701855925b1b05528bb60a45a1aa
Samuel <samuel.abreu2511@gmail.com>
2024-11-20 10:56:54 -0300

    ...

--- src/Pages/Doubt.jsx
+++ src/Pages/Doubt.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Header} from '../components/Header/Header'
-import { Cadastro } from '../Screens/Doubt/ScreenForm/Cadastro'
+import { Cadastro } from '../Screens/Doubt/ScreenForm/Cadastro.jsx'
 
 
 export const Doubt = () => {
