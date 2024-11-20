4f85e6034635ad6a895a79910897eb4268ebb0af
Samuel <samuel.abreu2511@gmail.com>
2024-11-20 11:38:13 -0300

    .

--- src/Pages/Doubt.jsx
+++ src/Pages/Doubt.jsx
@@ -3,7 +3,6 @@ import {Header} from '../components/Header/Header'
 import { Cadastro } from '../Screens/Doubt/ScreenForm/Cadastro'
 
 
-
 export const Doubt = () => {
   return (
     <>
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src', // Facilita imports
+      '@/': '/src', 
     },
   },
 });
