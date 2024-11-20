27dc8c118b48a8d4fcf1dcaa66394566a29fe3c1
Samuel <samuel.abreu2511@gmail.com>
2024-11-20 10:40:55 -0300

    testando

--- src/Pages/Doubt.jsx
+++ src/Pages/Doubt.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import {Header} from '../components/Header/Header'
-import { Formulario } from '../Screens/Doubt/ScreenForm/Formulario'
-
-
+import Formulario from '../Screens/Doubt/ScreenForm/Formulario';
 
 export const Doubt = () => {
   return (
