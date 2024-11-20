8b0c786c0e4d6072fd57593b7658d90319c18cf9
Samuel <samuel.abreu2511@gmail.com>
2024-11-20 16:31:27 -0300

    alteração na pasta

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import { Header } from '../components/Header/Header'
 import { Main } from '../Screens/AboutScreen/Meio/Main'
 import { ComoSurgiu } from '../Screens/AboutScreen/ComoSurgiu/ComoSurgiu'
-import { Footer } from '../components/Footer/Footer'
 import { SaudeMente } from '../Screens/AboutScreen/SaudeMente/SaudeMente'
 
 export const About = () => {
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import { Header } from '../components/Header/Header'
 import { Especialidades } from '../Screens/Especialidades/Especialidades'
 import { Apresentação } from '../Screens/Apresentação/Apresentação'
 import { Demandas } from '../Screens/Demandas/Demandas'
-import { Footer } from '../components/Footer/Footer'
 
 export const Home = () => {
   return (
