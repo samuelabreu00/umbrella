6b00495f783c683b69bb43b1742d4d17e54db66c
Samuel <samuel.abreu2511@gmail.com>
2024-11-20 17:31:41 -0300

    teste erro f5

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  plugins: [react()],
   server: {
-    historyApiFallback: true,  
+    historyApiFallback: true,  // Adiciona o fallback para o React Router
   },
-  base: '/',
+  resolve: {
+    alias: {
+      '@/': '/src',
+    },
+  },
 });
