be01a986b0051850e69a9e8467fac56f6f53e95f
Samuel <samuel.abreu2511@gmail.com>
2024-11-20 17:28:08 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@/': '/src', 
-    },
+  server: {
+    historyApiFallback: true,  
   },
+  base: '/',
 });
