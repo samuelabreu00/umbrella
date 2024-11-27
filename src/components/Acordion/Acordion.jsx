36f9adf3d63d3e5f5c127d8fd0a91a89447388c8
Samuel <samuel.abreu2511@gmail.com>
2024-11-27 13:01:51 -0300

    correção de erros

--- src/components/Acordion/Acordion.jsx
+++ src/components/Acordion/Acordion.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import './acordion.css';
+import './Acordion.css';
 import { FaChevronUp, FaChevronDown } from 'react-icons/fa';
 
 function Acordion({ titulo, texto }) {
