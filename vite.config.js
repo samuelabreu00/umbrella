875ba266ed4951d2f61227faa2473521e96fd33f
Samuel <samuel.abreu2511@gmail.com>
2024-11-29 12:27:53 -0300

    add robots

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/robots.txt', // Localização do seu arquivo `robots.txt`
+          dest: '', // Copia para a raiz da pasta `dist`
+        },
+      ],
+    }),
+  ],
   server: {
-    historyApiFallback: true,  // Adiciona o fallback para o React Router
+    historyApiFallback: true, // Adiciona o fallback para o React Router
   },
   resolve: {
     alias: {
